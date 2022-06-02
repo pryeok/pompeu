61c713f148131ff3cb4ddd1496f622af9388ae7e
pryeok <pryeok@gmail.com>
2022-06-02 23:19:27 +0900

    security

--- app/src/main/java/com/pompeu/security/service/PrincipalDetailService.java
+++ app/src/main/java/com/pompeu/security/service/PrincipalDetailService.java
@@ -64,6 +64,9 @@ public class PrincipalDetailService implements UserDetailsService {
 
 
 
+
+
+
   //       .orElseThrow(() -> {
   //         return new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다.:"+email);
   //       });
